a182d1fe0b64ef9af7285dff8c6861562b3124c2
Hussein <hosainalandaleb@gmail.com>
2018-04-12 21:47:34 +0300

    Hussein-Phone

---
+++ jscript/js.js
@@ -0,0 +1,31 @@
+
+
+var form = document.getElementById("c");
+
+$(document).ready(function(){
+        $(form).hide();
+});
+
+
+function a(){
+$(document).ready(function(){
+    $("#ii").click(function(){
+        $(form).show(1000);
+    });
+});
+    
+};
+
+var x = document.getElementById("sub-form");
+x.onclick = function(){
+    alert(" ^-^ ضحكت عليك صقيقي اني لو عندي ايفون اكس اباوع بخلققتك !!؟");
+}
+
+
+var exitlink = document.getElementById("exit-link");
+
+exitlink.onclick=function(){
+        $(form).hide(1000);
+}
+
+
